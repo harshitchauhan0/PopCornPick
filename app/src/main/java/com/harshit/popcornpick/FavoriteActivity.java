7c61225d9e863fd3a275006fff8f68eb96eeee45
Harshit <imailharshit@gmail.com>
2023-10-21 17:30:35 +0530

    Final Commit

--- app/src/main/java/com/harshit/popcornpick/FavoriteActivity.java
+++ app/src/main/java/com/harshit/popcornpick/FavoriteActivity.java
@@ -11,6 +11,6 @@ public class FavoriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
-        Button button;
+        Button button = findViewById(R.id.btn);
     }
 }
